50ac914f379e5481b3731159b8042fbbccc250d9
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-25 23:24:26 -0600

    [add] layout

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -4,14 +4,14 @@
 	<title>Laravel</title>
 </head>
 <body>
-
 	<h2>Notes</h2>
 	<ul>
 		@foreach($notes as $note)
-			<li>{{ $note->note }}</li>
+
+			<li>
+				{{ $note->note }}
+			</li>
 		@endforeach
 	</ul>
-
-
 </body>
 </html>
