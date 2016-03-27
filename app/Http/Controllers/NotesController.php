f08c9e240edb9da3e654c7be09eb3d91503327ef
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-26 23:18:05 -0600

    [update] notes controller

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use \App\Note
+use \App\Note;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -11,8 +11,22 @@ class NotesController extends Controller
 {
    public function index()
    {
-     $notes =Note::all();
+        $notes =Note::all();
+        return view('notes/list',compact('notes'));
+   }
 
-    return view('notes/list',compact('notes'));
+   public function create()
+   {
+        return view('notes/create');
+   }
+
+   public function store()
+   {
+        return 'Creating a note';
+   }
+
+   public function show($note)
+   {
+        dd($note);
    }
 }
