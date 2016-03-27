1c9bc3b0614ae7a478d1d64f7fed653fedd6c62c
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-26 23:24:52 -0600

    [update] routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,19 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('notes',function(){
+Route::get('notes','NotesController@index');
 
-	
-});
+Route::post('notes','NotesController@store');
+Route::get('notes/create','NotesController@create');
 
-Route::post('notes',function(){
-
-	return 'Creating a note';
-});
-
-
-Route::get('notes/create', function(){
-	return view('notes/create');
-});
+Route::get('notes/{note}','NotesController@show')->where('note','[0-9]+');
 
 
