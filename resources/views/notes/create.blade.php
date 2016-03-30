03baccbd42cead95a9684ec016ae75eb125579af
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-29 23:14:02 -0600

    [add] form notes

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,5 +1,11 @@
 @extends('layout')
 
 @section('content')
-<h1>Create a note</h1>
+	<h1>Create a note</h1>
+	<form method="post" action="{{ url('notes') }}">
+		{!! csrf_field() !!}
+
+		<textarea name="note"></textarea>
+	</form>
+
 @endsection
