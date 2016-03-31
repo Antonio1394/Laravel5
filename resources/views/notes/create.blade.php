d105bf710e2b37fabb718cfe5886bb541e4ff033
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-30 23:51:52 -0600

    [add] file errors

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -2,20 +2,12 @@
 
 @section('content')
 	<h1>Create a note</h1>
-	@if (! $errors->isEmpty())
-	<div class="alert alert-danger">
-		<p><strong>Opps!</strong> Please fix the following errors:</p>
-		<ul>
-			@foreach($errors->all() as $error)
-				<li>{{ $error }}</li>
-			@endforeach
-		</ul>
-	</div>
-	@endif
+	@include('partials/errors')
 	<form method="post" action="{{ url('notes') }}" class="form">
 		{!! csrf_field() !!}
-
-		<textarea name="note" class="form-control">{{ old('note') }}</textarea>
+		<div class="form-group">
+			<textarea name="note" class="form-control">{{ old('note') }}</textarea>
+		</div>
 		<br>
 		<button type="submit" class="btn btn-primary">Create note</button>
 	</form>
