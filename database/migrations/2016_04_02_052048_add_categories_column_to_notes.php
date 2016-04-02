2c2d388dbc003780a04a5f338218e16e2f851852
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-01 23:39:40 -0600

    [update] migration

--- database/migrations/2016_04_02_052048_add_categories_column_to_notes.php
+++ database/migrations/2016_04_02_052048_add_categories_column_to_notes.php
@@ -13,7 +13,7 @@ class AddCategoriesColumnToNotes extends Migration
     public function up()
     {
         Schema::table('notes', function (Blueprint $table) {
-            $table->unsignedInteger('category_id')->nullable()->index();
+            $table->unsignedInteger('category_id')->nullable()->index()->after('note');
         });
     }
 
