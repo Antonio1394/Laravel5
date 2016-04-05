579daedf9f3522d175eea6716acbeb5f5145eddc
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-04 23:04:53 -0600

    [update] view list note

--- resources/views/notes/list.blade.php
+++ resources/views/notes/list.blade.php
@@ -5,10 +5,10 @@
 	<p>
 		<a href="{{ url('notes/create') }}">Add a note</a>
 	</p>
-	<ul>
+	<ul class="list-group">
 		@foreach($notes as $note)
 
-			<li>
+			<li class="list-group-item">
 				{{ $note->note }}
 			</li>
 		@endforeach
