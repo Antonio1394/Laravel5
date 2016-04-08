5537e70deb58f32f23d3ca606dd522ce4795d063
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-07 23:18:21 -0600

    [add] model factory user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,10 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\User::class, 49)->create();
+        factory(App\User::class)->create([
+        	'name' => 'Antonio',
+        	'role' => 'admin'
+        	]);
     }
 }
