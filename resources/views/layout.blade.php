23247de79638f6327989e834008edb9e01774a90
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-08 23:05:02 -0600

    [add] layout login

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,9 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Laravel</title>
 
-    {!! Html::style('css/app.css') !!}
+  
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
   <nav class="navbar navbar-default">
