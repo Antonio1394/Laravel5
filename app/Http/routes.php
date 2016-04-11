da090db7be5abb6e549f9e76316edf816d82031d
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-10 22:09:09 -0600

    [add] password reset route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 // Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::get('login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
@@ -24,12 +24,19 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+//////////////////////////////////////
+
 
 Route::get('notes','NotesController@index');
-
 Route::post('notes','NotesController@store');
 Route::get('notes/create','NotesController@create');
-
 Route::get('notes/{note}','NotesController@show')->where('note','[0-9]+');
 
 
