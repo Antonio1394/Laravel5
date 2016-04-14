087bcdca0ee630d7848326a11410a41fef75616c
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-13 22:53:46 -0600

    [update] controller save use

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -55,11 +55,14 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user new User([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+        $user->role='user';
+        $user->save();
+        return $user;
     }
 
      public function loginPath()
